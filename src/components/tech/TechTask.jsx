ff86523bb008918b866d663bda2053226427be2b
nvduy248 <115880648+NvDenis@users.noreply.github.com>
2024-05-29 19:49:15 +0700

    update TechTask

--- src/components/tech/TechTask.jsx
+++ src/components/tech/TechTask.jsx
@@ -4,19 +4,19 @@ export const TechTask = () => {
   return (
     <div className='techs'>
       <p>Tech Task</p>
-      
+
       <div className="logos">
         <ul>
-          <li>
+          <li className='logo-1'>
             <img src={'https://skillicons.dev/icons?i=html,css'} alt="" />
           </li>
-          <li>
+          <li className='logo-2'>
             <img src={'https://skillicons.dev/icons?i=js,ts'} alt="" />
           </li>
-          <li>
+          <li className='logo-3'>
             <img src={'https://skillicons.dev/icons?i=react,nextjs'} alt="" />
           </li>
-          <li>
+          <li className='logo-4'>
             <img src={'https://skillicons.dev/icons?i=tailwind,sass'} alt="" />
           </li>
         </ul>
