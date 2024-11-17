c2999a5596a1964e407e01af29fb642f94936784
denis <vduyit@gmail.com>
2024-11-17 08:30:42 +0700

    Update ThemeContext.jsx

--- src/contexts/ThemeContext.jsx
+++ src/contexts/ThemeContext.jsx
@@ -3,7 +3,7 @@ import { createContext, useState, useEffect } from "react";
 export const ThemeContext = createContext();
 
 export const ThemeProvider = ({ children }) => {
-  const [theme, setTheme] = useState(localStorage.getItem("theme") || "light");
+  const [theme, setTheme] = useState(localStorage.getItem("theme") || "dark");
 
   useEffect(() => {
     document.documentElement.setAttribute("data-theme", theme);
